9b845834f79fff3c93313009a8a47066cbccf049
Tionni <johorali2544@gmail.com>
2024-12-17 05:24:11 +0300

    check if it is weekend

--- lesson-15/15a.js
+++ lesson-15/15a.js
@@ -1,4 +1,5 @@
 import dayjs from 'https://unpkg.com/dayjs@1.11.10/esm/index.js'
+import { isWeekend } from './15e.js'
 const today = dayjs()
 const fiveDaysLater = today.add("5", "days")
 const amonthLater = today.add("1", "months")
@@ -7,6 +8,9 @@ const aMonthBefore = today.subtract("1", "months")
 const datestring = fiveDaysLater.format('MMMM D')
 const datestring2 = amonthLater.format('MMMM D')
 const datestring3 = aMonthBefore.format('MMMM D')
+
+
 console.log(datestring)
 console.log(datestring2)
 console.log(datestring3)
+console.log(isWeekend(today))
