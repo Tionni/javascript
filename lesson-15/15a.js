81e231a14b4480d269e244a3f0e718aac8f95ec9
Tionni <johorali2544@gmail.com>
2024-12-17 05:00:38 +0300

    calculated a month before date

--- lesson-15/15a.js
+++ lesson-15/15a.js
@@ -2,8 +2,11 @@ import dayjs from 'https://unpkg.com/dayjs@1.11.10/esm/index.js'
 const today = dayjs()
 const fiveDaysLater = today.add("5", "days")
 const amonthLater = today.add("1", "months")
+const aMonthBefore = today.subtract("1", "months")
 
 const datestring = fiveDaysLater.format('MMMM D')
 const datestring2 = amonthLater.format('MMMM D')
+const datestring3 = aMonthBefore.format('MMMM D')
 console.log(datestring)
 console.log(datestring2)
+console.log(datestring3)
